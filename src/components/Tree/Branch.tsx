e750f001ffb70fa06bdeb31de141ee67a2212d44
Shubham Kulkarni <58355613+shubhkulkarni@users.noreply.github.com>
2024-09-27 09:13:35 +0000

    added keys

--- src/components/Tree/Branch.tsx
+++ src/components/Tree/Branch.tsx
@@ -9,18 +9,21 @@ interface BranchProps {
 }
 
 const Branch: React.FC<BranchProps> = ({ name, root }: BranchProps) => {
-  const [open, setOpen] = useState<boolean>(true);
+  const [isOpen, setIsOpen] = useState<boolean>(true);
+
   const toggleOpen = useCallback(() => {
-    setOpen((prev) => !prev);
+    setIsOpen((prev) => !prev);
   }, []);
 
   return (
     <div className="flex flex-col items-start justify-start">
       <div className="name flex justify-start items-center gap-2">
-        <button onClick={toggleOpen}>{open ? "-" : "+"}</button>
+        <button type="button" onClick={toggleOpen}>
+          {isOpen ? "-" : "+"}
+        </button>
         <div className="name">{name}</div>
       </div>
-      {open && (
+      {isOpen && (
         <div className="content ml-4">
           <Tree root={root} />
         </div>
--- src/components/Tree/hooks.tsx
+++ src/components/Tree/hooks.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useMemo } from "react";
+import { useCallback, useMemo, useState } from "react";
 import { objectType, primitive } from "./types";
 import Branch from "./Branch";
 import Leave from "./Leave";
@@ -6,19 +6,24 @@ import Leave from "./Leave";
 export const useTree = (root: objectType) => {
   const tree = useMemo(
     () =>
-      Object.entries(root).map((i) => {
+      Object.entries(root).map((i, idx) => {
         if (typeof i[1] === "object") {
           let val = i[1];
           if (Array.isArray(i[1])) val = objectifyArr(val as unknown[]);
-          return <Branch name={i[0]} root={val as objectType} />;
-        } else return <Leave name={i[0]} value={i[1] as primitive} />;
+          return (
+            <Branch key={idx + i[0]} name={i[0]} root={val as objectType} />
+          );
+        } else
+          return (
+            <Leave key={idx + i[0]} name={i[0]} value={i[1] as primitive} />
+          );
       }),
-    []
+    [root]
   );
 
   const renderTree = useCallback(() => {
     return <div className="tree">{tree}</div>;
-  }, []);
+  }, [tree]);
 
   return renderTree;
 };
